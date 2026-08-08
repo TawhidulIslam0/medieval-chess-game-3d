e99bde4ecd67b123bccb52642730c3c49a4cc06d
TawhidulIslam0 <islam100tawhidul@gmail.com>
2026-08-08 15:49:13 -0400

    feat: update base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/medieval-chess-game-3d/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
